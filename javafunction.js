cb5e4c957061aa7a6c725ec33cb3744355648f78
evelinruizmoya <evelinruizmoya@gmail.com>
2026-02-17 21:13:24 -0500

    changes

--- javafunction.js
+++ javafunction.js
@@ -57,17 +57,49 @@ function addMultiplesToArray() {
 
 // **Objects and Form Fields**
 
+function printCarObject(){
+    let carType = document.getElementById("carType").value;
+    let carMPG = document.getElementById("carMPG").value;
+    let carColor = document.getElementById("carColor").value;
 
+    let carObject = {
+        cType: carType,
+        cMPG: carMPG,
+        cColor: carColor
+    };
 
-
+    console.log(carObject);
+}
 
 // **Objects and Form Fields pt. 2**
+function loadCar(num) {
+    let car;
+    if (num ===1) {
+        car = carObject1;
+    } 
+    else if (num === 2) {
+        car = carObject2;
+    } 
+    else if (num === 3) {
+        car = carObject3;
+  }
 
-
-
+  document.getElementById("carType").value = car.cType;
+  document.getElementById("carMPG").value = car.cMPG;
+  document.getElementById("carColor").value = car.cColor;
+}
 
 // **Changing Styles**
+function changeColor(choice){
+    let styleParagraph = document.getElementById("styleParagraph");
 
-
-
-
+    if (choice === 1) {
+        styleParagraph.style.color = "red";
+    } 
+    else if (choice === 2) {
+        styleParagraph.style.color = "green";
+    } 
+    else if (choice === 3) {
+        styleParagraph.style.color = "blue";
+    }
+}
